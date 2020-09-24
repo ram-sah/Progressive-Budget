ce4f34a8190c55700909c10a3cb072b6e498f738
Ram Sah <mohan2036@gmail.com>
2020-09-24 02:30:30 -0400

    deploying

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.static("public"));
 //   useNewUrlParser: true,
 //   useFindAndModify: false
 // });
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
